8af9292850f15e5158b897ef36235329ba7847d9
zakky0402 <shogo.okazaki@linkode.co.jp>
2021-05-10 23:57:01 +0900

    [update]Googleアナリティクスの設定変更

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,6 +15,13 @@ module.exports = {
     },
   },
   plugins: [
+    {
+      resolve: "gatsby-plugin-google-analytics",
+      options: {
+        trackingId: process.env.GOOGLE_ANALYTICS_TRACKING_ID,
+        head: true,
+      },
+    },
     `gatsby-plugin-image`,
     {
       resolve: `gatsby-source-filesystem`,
@@ -174,12 +181,6 @@ module.exports = {
     `gatsby-plugin-gatsby-cloud`,
     `gatsby-plugin-typegen`,
     `gatsby-plugin-material-ui`,
-    {
-      resolve: "gatsby-plugin-google-analytics",
-      options: {
-        trackingId: process.env.GOOGLE_ANALYTICS_TRACKING_ID,
-      },
-    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
