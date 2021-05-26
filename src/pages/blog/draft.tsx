93146f96753c3409ddeec1e6c74b945cdcf489de
zakky0402 <shogo.okazaki@linkode.co.jp>
2021-05-27 00:26:45 +0900

    [add]下書きページを新規作成

---
+++ src/pages/blog/draft.tsx
@@ -0,0 +1,49 @@
+import React, { useEffect, useState } from "react"
+import { PageProps } from "gatsby"
+import queryString from "query-string"
+
+import Layout from "../../components/Layout"
+import SEO from "../../components/SEO"
+import PostContent from "../../components/Organisms/PostContent"
+
+const BlogPage: React.FC<PageProps<GatsbyTypes.BlogPageQuery>> = ({
+  location,
+}) => {
+  const { contentId, draftKey } = queryString.parse(location.search)
+  const [data, setData] = useState<GatsbyTypes.BlogPageQuery | null>(null)
+
+  useEffect(() => {
+    fetch(
+      `https://your-service-id.microcms.io/api/v1/post/${contentId}?draftKey=${draftKey}`,
+      {
+        headers: {
+          "X-API-KEY": "your-api-key",
+        },
+      }
+    )
+      .then(res => res.json())
+      .then(res => setData({ microcmsPost: res })) // ※1
+  }, []) // ※2
+
+  if (data === null) {
+    return null
+  }
+
+  return (
+    <Layout>
+      <SEO
+        title={data.microcmsPost?.title}
+        description={data.microcmsPost?.description}
+        image={data.microcmsPost?.ogimage?.url}
+      />
+      <PostContent
+        title={data.microcmsNews?.title}
+        publishedAt={data.microcmsNews?.publishedAt}
+        updatedAt={data.microcmsNews?.updatedAt}
+        content={data.microcmsNews?.content}
+      />
+    </Layout>
+  )
+}
+
+export default BlogPage
